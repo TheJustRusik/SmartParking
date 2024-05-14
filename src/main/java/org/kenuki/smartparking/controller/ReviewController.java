71d4319961c846fa5bb98dd9d7e30c4561edbb18
TheJustRusik <7thejustrusik7@gmail.com>
2024-05-14 09:54:03 +0500

    Cross for reviews

--- src/main/java/org/kenuki/smartparking/controller/ReviewController.java
+++ src/main/java/org/kenuki/smartparking/controller/ReviewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/review")
 @AllArgsConstructor
+@CrossOrigin
 public class ReviewController {
     private final ReviewService reviewService;
     @GetMapping("/{id}")
